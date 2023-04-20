de2c6c1262f10b272a464016e9cae2e49496ef14
Robert Arnosson <rha006@outlook.com>
2023-04-20 12:39:50 +0200

    Added some fancyness

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("mousemove", (event) => {
+    const x = (event.clientX / window.innerWidth) * 100;
+    const y = (event.clientY / window.innerHeight) * 100;
+    document.documentElement.style.setProperty('--diagonal-x', `${x}%`);
+    document.documentElement.style.setProperty('--diagonal-y', `${y}%`);
+    console.log(`Mouse position: (${x}%, ${y}%)`);
+  });
+
+document.getElementById('arrow-down-id').addEventListener('click', () => {
+    document.getElementById('diagonal').scrollIntoView({behavior: 'smooth'})
+})
